e319776ac7bf6d0d6133fe8275c68a455b33c6ce
Shydeev <hday5297@gmail.com>
2023-05-18 11:25:00 +0300

    добавить страницу описания книги.

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -4,9 +4,12 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         { 
-            new Book(1, "ISBN 12312-31231", "D.Knuth", "Art of Programming"),
-            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring"),
-            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language")
+            new Book(1, "ISBN 12312-31231", "D.Knuth", "Art of Programming", 
+                "This volume begins with basic programming concepts...", 7.19m),
+            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring",
+                "As the application of object technology-particularly the Java...", 12.45m),
+            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language",
+                "Known as the Bible of C, this classic bestseller...", 14.98m)
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -20,6 +23,11 @@ namespace Store.Memory
             return books.Where(book => book.Author.Contains(query)
                                     || book.Title.Contains(query))
                 .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
