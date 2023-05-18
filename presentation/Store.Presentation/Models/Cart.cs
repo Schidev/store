8beea1aa54c5007f904dc4f70cbf2ad49f86a4ee
Shydeev <hday5297@gmail.com>
2023-05-18 12:30:06 +0300

    добавить книгу в корзину.

---
+++ presentation/Store.Presentation/Models/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Presentation.Models
+{
+    public class Cart
+    {
+        public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
+        public decimal Amount { get; set; }
+    }
+}
