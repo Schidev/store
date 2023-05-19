b1b89fa9d279ac308d3b37204522a024c3d0272e
Shydeev <hday5297@gmail.com>
2023-05-19 22:04:21 +0300

    добавить заказ и хранилище заказов.

--- presentation/Store.Presentation/Controllers/CartController.cs
+++ presentation/Store.Presentation/Controllers/CartController.cs
@@ -6,31 +6,36 @@ namespace Store.Presentation.Controllers
     public class CartController : Controller
     {
         private readonly IBookRepository bookRepository;
+        private readonly IOrderRepository orderRepository;
 
-        public CartController(IBookRepository bookRepository)
+        public CartController(IBookRepository bookRepository, IOrderRepository orderRepository)
         {
             this.bookRepository = bookRepository;
+            this.orderRepository = orderRepository;
         }
+
         public IActionResult Add(int id)
         {
-            var book = bookRepository.GetById(id);
+            Order order;
             Cart cart;
 
-            if (!HttpContext.Session.TryGetCart(out cart)) 
+            if (HttpContext.Session.TryGetCart(out cart))
             {
-                cart = new Cart();
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else 
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
             }
 
-            if (cart.Items.ContainsKey(id))
-            {
-                cart.Items[id]++;
-            }
-            else
-            {
-                cart.Items[id] = 1;
-            }
+            var book = bookRepository.GetById(id);
+            order.AddItem(book, 1);
 
-            cart.Amount += book.Price;
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
 
             HttpContext.Session.Set(cart);
 
